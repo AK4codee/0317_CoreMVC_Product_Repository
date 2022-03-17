714cabadcf898541e1130f21938d87e00ea8c212
郭家秀 <a880318@gmail.com>
2022-03-17 14:07:13 +0800

    feat: add DBRepository

---
+++ 0317_Product_Repository/Repositories/DBRepository.cs
@@ -0,0 +1,43 @@
+﻿using _0317_Product_Repository.Models.DBEntity;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace _0317_Product_Repository.Repositories
+{
+    public class DBRepository
+    {
+        private readonly ProductsDBContext _context;
+
+        public DBRepository(ProductsDBContext context)
+        {
+            _context = context;
+        }
+
+        public ProductsDBContext Context { get { return _context; } }
+
+        public virtual void Create<T>(T entity) where T : class
+        {
+            _context.Entry(entity).State = EntityState.Added;
+        }
+
+        public virtual void Update<T>(T entity) where T : class
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public virtual void Delete<T>(T entity) where T : class
+        {
+            _context.Entry(entity).State = EntityState.Deleted;
+        }
+
+        public IQueryable<T> GetAll<T>(T entity) where T : class
+        {
+            return _context.Set<T>();
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
