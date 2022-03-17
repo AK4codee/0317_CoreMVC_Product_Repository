dc4c10e01bd1cf2371a0300345db0bc356fc9586
郭家秀 <a880318@gmail.com>
2022-03-17 14:14:23 +0800

    feat: 繼承 DBRepository > 做出客製方法

---
+++ 0317_Product_Repository/Repositories/ProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using _0317_Product_Repository.Models.DBEntity;
+using System.Linq;
+
+namespace _0317_Product_Repository.Repositories
+{
+    public class ProductRepository : DBRepository
+    {
+        public ProductRepository(ProductsDBContext context) : base(context) { }
+
+        public void DeleteById(int id)
+        {
+            var target = Context.Products.First(x => x.Id == id);
+            Delete(target);
+
+            Save();
+        }
+    }
+}
