403de9b562b35d5a4bda3c743544fe76464b18dc
郭家秀 <a880318@gmail.com>
2022-03-19 11:42:09 +0800

    feat: Repository Interface & Product Repository

---
+++ 0317_Product_Repository/Repositories/DBRepository.cs
@@ -0,0 +1,43 @@
+﻿using _0317_Product_Repository.Models.DBEntity;
+using _0317_Product_Repository.Repositories.Interface;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace _0317_Product_Repository.Repositories
+{
+    public class DBRepository : IDBRepository
+    {
+        private readonly ProductsDBContext _context;
+
+        public DBRepository(ProductsDBContext context)
+        {
+            _context = context;
+        }
+        public ProductsDBContext DbContext { get { return _context; } }
+
+        public void Create<T>(T entity) where T : class
+        {
+            _context.Entry(entity).State = EntityState.Added;
+        }
+
+        public void Delete<T>(T entity) where T : class
+        {
+            _context.Entry(entity).State = EntityState.Deleted;
+        }
+
+        public IQueryable<T> GetAll<T>() where T : class
+        {
+            return _context.Set<T>();
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Update<T>(T entity) where T : class
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+    }
+}
---
+++ 0317_Product_Repository/Repositories/Interface/IDBRepository.cs
@@ -0,0 +1,17 @@
+﻿using _0317_Product_Repository.Models.DBEntity;
+using System.Linq;
+
+namespace _0317_Product_Repository.Repositories.Interface
+{
+    public interface IDBRepository
+    {
+        public ProductsDBContext DbContext { get; }
+
+        public void Create<T>(T entity) where T : class;
+        public void Update<T>(T entity) where T : class;
+        public void Delete<T>(T entity) where T : class;
+        public IQueryable<T> GetAll<T>() where T : class;
+        public void Save();
+
+    }
+}
