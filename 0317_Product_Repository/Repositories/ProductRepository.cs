ac84e32e587360eabfb368234fbc9970ff880a5b
郭家秀 <a880318@gmail.com>
2022-03-22 16:36:46 +0800

    feat: add ProductRepository and Interface

---
+++ 0317_Product_Repository/Repositories/ProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using _0317_Product_Repository.Models.DBEntity;
+using _0317_Product_Repository.Repositories.Interface;
+using System.Linq;
+
+namespace _0317_Product_Repository.Repositories
+{
+    public class ProductRepository : DBRepository, IProductRepository
+    {
+        public ProductRepository(ProductsDBContext context) : base(context)
+        {
+        }
+
+        public void DeleteById(int id)
+        {
+            var target = Context.Products.First(x => x.Id == id);
+            
+            Delete(target);
+            Save();
+        }
+    }
+}
